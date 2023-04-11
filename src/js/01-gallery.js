bc4a29356bcf9a10b78ba5971e248d194c8e2662
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-04-11 14:27:28 +0200

    1 task completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+const createGalleryList = galleryItems
+    .map(
+        ({ preview, original, description }) =>
+            `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>`
+        ).join("");
+
+gallery.innerHTML = createGalleryList;
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
